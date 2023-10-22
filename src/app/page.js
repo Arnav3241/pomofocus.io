5a7d141143ab9249848f176a67479589a8b13b66
Arnav3241 <arnav.singh.legendary.202@gmail.com>
2023-10-22 16:31:09 +0530

    Added Header!

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,38 @@
+import Image from 'next/image'
+
 export const metadata = {
   title: 'Pomofocus.io',
   description: "Arnav's Pomodoro Timer",
 }
 
-export default function Home() {
+export default function todo() {
+  var timePercenteage = 60;
+
   return (
-    <div className="p-10">
-        <div className="text-4xl">
-          Todo: Add Pomodoro Timer
+    <div className="h-[100vh] w-[100vw] bg-[#BA4949] flex justify-center align-middle">
+      <div className="h-[8vh] w-[330px]">
+        <div className="w-full h-[7vh] flex justify-between">
+          <div className="flex items-center">
+            <Image src="/icon.png" width={10} height={10} className='w-[12px] h-[12px]' />
+            <div className="px-1 text-[12px] font-semibold text-white"> Pomofocus </div>
+          </div>
+          <div className="flex items-center buttons">
+            <button className="p-1 px-2 rounded bg-[#d45959] text-[8px] mx-1 text-semibold text-white flex items-center">
+              <Image src="/report.png" width={10} height={10} className='w-[9px] h-[9px]' /> 
+              <div className='pl-1'>Report</div>
+            </button>
+            <button className="p-1 px-2 rounded bg-[#d45959] text-[8px] mx-1 text-semibold text-white flex items-center">
+              <Image src="/settings.png" width={10} height={10} className='w-[9px] h-[9px]' /> 
+              <div className='pl-1'>Settings</div>
+            </button>
+          </div>
         </div>
-        <ul className="list-disc px-10 pt-5">
-          <li className="w-full">Add a basic timer</li>
-          <li className="w-full">Improve the design.</li>
-          <li className="w-full">Add a to-do section.</li>
-          <li className="w-full">Add a pop up modal</li>
-          <li className="w-full">Add a report section.</li>
-          <li className="w-full">Add a goals section.</li>
-          <li className="w-full">Add different sections.</li>
-          <li className="w-full">Add a settings section.</li>
-          <li className="w-full">Make a mongo db database to store user's data.</li>
-          <li className="w-full">Make a mobile compatable version</li>
-          <li className="w-full">Host it on free hosting service like netlify.</li>
-          <li className="w-full">Make a activity summary section</li>
-          <li className="w-full">Add a dark theme.</li>
-        </ul>
+        <div className="w-full h-[1vh]">
+          <div className='bg-[#d45959] w-full h-[0.3vh]'>
+            <div className={`bg-white w- h-[0.3vh]`}></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/app/todo/page.js
@@ -0,0 +1,30 @@
+export const metadata = {
+    title: 'Pomofocus.io',
+    description: "Todo",
+  }
+  
+  export default function Home() {
+    return (
+      <div className="p-10">
+          <div className="text-4xl">
+            Todo: Add Pomodoro Timer
+          </div>
+          <ul className="list-disc px-10 pt-5">
+            <li className="w-full">Add a basic timer</li>
+            <li className="w-full">Improve the design.</li>
+            <li className="w-full">Add a to-do section.</li>
+            <li className="w-full">Add a pop up modal</li>
+            <li className="w-full">Add a report section.</li>
+            <li className="w-full">Add a goals section.</li>
+            <li className="w-full">Add different sections.</li>
+            <li className="w-full">Add a settings section.</li>
+            <li className="w-full">Make a mongo db database to store user's data.</li>
+            <li className="w-full">Make a mobile compatable version</li>
+            <li className="w-full">Host it on free hosting service like netlify.</li>
+            <li className="w-full">Make a activity summary section</li>
+            <li className="w-full">Add a dark theme.</li>
+          </ul>
+      </div>
+    )
+  }
+  
